07faf75d7f2b1fb26b02e98c144822fef1cb5a18
yez <fzy09@outlook.com>
2025-02-10 20:33:26 +0800

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site:"https://jovial-cheesecake-33ae1d.netlify.app"
+  site:"https://jovial-cheesecake-33ae1d.netlify.app",
+  integrations: [preact()]
 });
